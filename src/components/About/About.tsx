6836a96b197473ada256bc90d749722b8a59d5b3
Bruno Catan <75697499+BrunoCatan@users.noreply.github.com>
2023-03-20 16:01:57 -0300

    Update About.tsx

--- src/components/About/About.tsx
+++ src/components/About/About.tsx
@@ -3,7 +3,7 @@ import img from "../../assets/Image01.svg";
 
 export function About() {
   const cv =
-    "https://drive.google.com/u/0/uc?id=1Bzv_K2tMpb83FCWdmKIcOzDuz-O2DJ6h&export=download";
+    "https://public.ph.files.1drv.com/y4mQ9zzJ5xiheCgX_db2kG3lMkrvlQKN0MXAWoc79wzmib2nadBQEJIdel1VBrpiZO1fzUigFDLzA5rCgjjo7nnvHqmNoaw0cx4x7qWWS4cLLopdydHLOzmxDxifT8GCQIo_bQUWyysxbhICrm0vLsguz2lT7ZjyPwLNByb8r3EEP6teMFudfjdyZR4_wax5jhSYa2te4xCkSUiTKNjnbtUX_OOJHefA4HWE4mczB-rb2s?AVOverride=1";
 
   return (
     <div>
