9ed8be6766c7853c7c2a64e1609472137880949b
brunocatan <brunocatandev@gmail.com>
2023-06-24 12:41:51 -0300

    update cv

--- src/components/About/About.tsx
+++ src/components/About/About.tsx
@@ -3,7 +3,7 @@ import img from "../../assets/Image01.svg";
 
 export function About() {
     const cv =
-        "https://fbuqcq.ph.files.1drv.com/y4mTQ5aiG43wbGROqNngYFMdXpDbxLED3NtjNZ9j22Y5loIWKgZDh2rVJ56tBWOcukLKh2H71eighXueQfq7OFnErY2yQ3M109H5VKrDsnPh3RsY0n62xSGOu5C_HS-X1PVBb88_KkWaEtU6E07jJqiImO_UAT9_mdicU1sP-qG9-snzBoMyQ6ZPjvRdqTPpsz2IOfqNILmV1dG4yPtR06Ofw";
+        "https://public.ph.files.1drv.com/y4msD2RYoC9TB6vbZKy8LOgNu1NR7bzxvghC6Z-jGOvx8T-eaUTkSUVS654T-8789AGBDN6dkG-vpAxLOss38kRcMmEmMWgx7WpF8vCshVb8OqfyCp8BWN7SeUrdh2eCiNhKz3cr3yFrZtvXKhZTWKqFHhEwV21eEc2-YTsu_cU1OVuR4L4-TI2apv26raNSJBtEUAEolC6oSeccHgMu_XxXXhJN8Brvl5LW_ukA4T1ocM?AVOverride=1";
 
     return (
         <div>
